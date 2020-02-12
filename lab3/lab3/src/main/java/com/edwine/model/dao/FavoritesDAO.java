3e98f748379f8a78e0b7b382895ad84b60205cc2
Pontus Backman <ponbac@student.chalmers.se>
2020-02-12 10:57:08 +0100

    added test for setting score

--- lab3/lab3/src/main/java/com/edwine/model/dao/FavoritesDAO.java
+++ lab3/lab3/src/main/java/com/edwine/model/dao/FavoritesDAO.java
@@ -7,6 +7,8 @@ package com.edwine.model.dao;
 
 import com.edwine.model.entity.Account;
 import com.edwine.model.entity.Favorites;
+import com.edwine.model.entity.QFavorites_;
+import easycriteria.JPAQuery;
 import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
@@ -28,6 +30,16 @@ public class FavoritesDAO extends AbstractDAO<Favorites> {
     
     public List<Account> findUsersMatchingName() {
         throw new UnsupportedOperationException("Not implemented yet!");
+    }
+    
+    public void setScore(Favorites favorite,int score) {
+        //QFavorites_ favorites = new QFavorites_();
+        
+        //List<Favorites> result = new JPAQuery(entityManager).select(favorites).where(favorites.account.username.like(favorite.getAccount().getUsername())).getResultList();
+        //List<Favorites> result = new JPAQuery(entityManager).select(favorites).where(favorites.account.username.like(favorite.getAccount().getUsername()).and(favorites.film.));
+        
+        favorite.setScore(score);
+        super.update(favorite);
     }
     
 }
--- lab3/lab3/src/test/java/com/edwine/model/entity/FavoritesDAOTest.java
+++ lab3/lab3/src/test/java/com/edwine/model/entity/FavoritesDAOTest.java
@@ -4,6 +4,8 @@ package com.edwine.model.entity;
 import com.edwine.model.dao.AccountDAO;
 import com.edwine.model.dao.FavoritesDAO;
 import com.edwine.model.dao.FilmDAO;
+import easycriteria.JPAQuery;
+import java.util.List;
 import javax.ejb.EJB;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
@@ -30,7 +32,7 @@ public class FavoritesDAOTest {
     @Deployment
 	public static WebArchive createDeployment() {
 		return ShrinkWrap.create(WebArchive.class)
-			.addClasses(FavoritesDAO.class, Favorites.class, Account.class, Film.class, AccountDAO.class)
+			.addClasses(FavoritesDAO.class, Favorites.class, Account.class, Film.class, AccountDAO.class, FilmDAO.class)
 			.addAsResource("META-INF/persistence.xml")
 			.addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
 	}
@@ -40,18 +42,37 @@ public class FavoritesDAOTest {
         @EJB
         private AccountDAO accountDAO;
         @EJB
-        //private FilmDAO filmDAO;
+        private FilmDAO filmDAO;
+        
+        Account account1;
+        Film film1;
+        Favorites favorite1;
 
 	@Before
 	public void init() {
-                //favoritesDAO.removeAll();
-                //favoritesDAO.create(new Favorites(new FavoritesKey("id:1234", "Pedds"), accountDAO, , 70));
+                favoritesDAO.removeAll();
+                
+                account1 = new Account("abc123", "Pontus");
+                film1 = new Film("ABC", 1998, "Action", "Edwin");
+                favorite1 = new Favorites(account1, film1, 7);
+                
+                accountDAO.create(account1);
+                filmDAO.create(film1);
+                favoritesDAO.create(favorite1);
+               
+                
                 //favoritesDAO.create(new Favorites());
                 //favoritesDAO.create(new Favorites());
 	}
 
 	@Test
-	public void checkThatFindCarsMatchingNameMatchesCorrectly() {
-		Assert.assertTrue(true); /* Some better condition */
+	public void checkThatScoreUpdatesCorrectly() {
+                Favorites favoriteBefore = favoritesDAO.findFavoritesMatchingFilmAndAccount(film1, account1);
+                
+                favoritesDAO.setScore(favorite1, 2);
+                
+                Favorites favoriteAfter = favoritesDAO.findFavoritesMatchingFilmAndAccount(film1, account1);
+            
+		Assert.assertTrue((favoriteBefore.getScore() != favoriteAfter.getScore()) && (favoriteAfter.getScore() == 2)); /* Some better condition */
 	}
 }
