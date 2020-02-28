04ab709aaf83affcc3ea0a4b1e894d0a7d036021
Pontus Backman <ponbac@student.chalmers.se>
2020-02-28 15:47:00 +0100

    favorite button now adds favortie to logged in account

--- lab3/lab3/src/main/java/com/edwine/model/FavoritesBean.java
+++ lab3/lab3/src/main/java/com/edwine/model/FavoritesBean.java
@@ -23,24 +23,31 @@ import omdb.model.SearchObject;
 @Named
 @ViewScoped
 public class FavoritesBean implements Serializable {
-    
+
     @EJB
     private FavoritesDAO favDAO;
-   
+
     @EJB
     private AccountDAO accDAO;
-    
+
     @EJB
     private FilmDAO filmDAO;
-    
-    public void addFavorite(SearchObject film, Account acc){
-        //favDAO.removeAll();
-        //accDAO.removeAll();
-        //accDAO.create(new Account("Pontus", "Pontus Backman", new HashSet<Favorites>()));
-        
-        Film f = filmDAO.findFilmsMatchingTitle(film.getTitle()).get(0);
-        favDAO.add(acc, f, 0);
-        
+
+    public void addFavorite(SearchObject film, String username) {
+        if (username != null) {
+            Account acc = accDAO.getAccountMatchingUsername(username);
+
+            if (acc == null) {
+                System.out.println("ERROR: Could not find logged in account!");
+            } else {
+                Film f = filmDAO.findFilmsMatchingTitle(film.getTitle()).get(0);
+                System.out.println("SUCCESS: " + f.getTitle() + " added as favorite for user " + acc.getUsername());
+                favDAO.add(acc, f, 0);
+            }
+        } else {
+            System.out.println("ERROR: No logged in user, can not add favorite!");
+        }
+
         //System.out.println("Favorites: " + favDAO.getAccountsWhoFavoritedFilm(f).get(0).getAccount());
     }
 }
