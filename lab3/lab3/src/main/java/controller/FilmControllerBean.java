4d906764dd7b0f5ca59f5a90bd39ff80eeb97ee0
Pontus Backman <ponbac@student.chalmers.se>
2020-03-04 14:56:45 +0100

    can now get more than 10 objects in a search

--- lab3/lab3/src/main/java/controller/FilmControllerBean.java
+++ lab3/lab3/src/main/java/controller/FilmControllerBean.java
@@ -25,37 +25,69 @@ import view.FilmBackingBean;
 @Named
 @ViewScoped
 public class FilmControllerBean implements Serializable {
-    
+
     private List<Film> mostRecentSearchResults;
-    
+
     @Inject
     private FilmBackingBean filmBackingBean;
-    
+
     @EJB
     private FilmDAO filmDAO;
+
+    public List<Film> searchFilms(int maxNumberOfResults) {
+        System.out.println(filmBackingBean.getSearchString());
+        //List<SearchObject> searchResults = OmdbService.getSearchObjectsFromSearchString(filmBackingBean.getSearchString());
+
+        List<SearchObject> searchResults = new ArrayList();
+
+        for (int i = 1; searchResults.size() < maxNumberOfResults; i++) {
+            List<SearchObject> tmpList = new ArrayList();
+
+            tmpList = OmdbService.getSearchObjectsFromSearchString(filmBackingBean.getSearchString(), i);
+            if (tmpList.size() == 0) {
+                maxNumberOfResults = 0;
+            }
+            
+            searchResults.addAll(tmpList);
+        }
+        
+        mostRecentSearchResults = new ArrayList();
+
+        for (SearchObject s : searchResults) {
+            try {
+                Film film = new Film(s.getImdbID(), s.getTitle(), s.getYear(), s.getType(), s.getPoster());
+                mostRecentSearchResults.add(film);
+                filmDAO.create(film);
+            } catch (Exception e) {
+                System.out.println("Error when adding ID to database, probably because the movie already exists in the database! " + e.getMessage());
+            }
+        }
+
+        System.out.println("Found " + searchResults.size() + " search results!");
+        return mostRecentSearchResults;
+    }
     
     public List<Film> searchFilms() {
         System.out.println(filmBackingBean.getSearchString());
         List<SearchObject> searchResults = OmdbService.getSearchObjectsFromSearchString(filmBackingBean.getSearchString());
-
+        
         mostRecentSearchResults = new ArrayList();
 
-            for (SearchObject s : searchResults) {
-                try {
-                    Film film = new Film(s.getImdbID(), s.getTitle(), s.getYear() ,s.getType(), s.getPoster());
-                    mostRecentSearchResults.add(film);
-                    filmDAO.create(film);
-                }catch (Exception e) {
-                    System.out.println("Error when adding ID to database, probably because the movie already exists in the database! " + e.getMessage());
-                }
+        for (SearchObject s : searchResults) {
+            try {
+                Film film = new Film(s.getImdbID(), s.getTitle(), s.getYear(), s.getType(), s.getPoster());
+                mostRecentSearchResults.add(film);
+                filmDAO.create(film);
+            } catch (Exception e) {
+                System.out.println("Error when adding ID to database, probably because the movie already exists in the database! " + e.getMessage());
             }
-            
-       
-        //push.send("newdata");
+        }
+
+        System.out.println("Found " + searchResults.size() + " search results!");
         return mostRecentSearchResults;
     }
-    
-    public List<Film> getSearchResult(){
+
+    public List<Film> getSearchResult() {
         return mostRecentSearchResults;
     }
 }
