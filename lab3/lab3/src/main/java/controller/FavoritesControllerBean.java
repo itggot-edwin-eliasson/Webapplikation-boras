1d564c2d16357e3aa72fc9ff05d035b72e81ca67
Pontus Backman <ponbac@student.chalmers.se>
2020-03-04 08:48:21 +0100

    removed all socket code and added method to remove favorite

--- lab3/lab3/src/main/java/controller/FavoritesControllerBean.java
+++ lab3/lab3/src/main/java/controller/FavoritesControllerBean.java
@@ -10,6 +10,7 @@ import com.edwine.model.dao.FavoritesDAO;
 import com.edwine.model.dao.FilmDAO;
 import com.edwine.model.entity.Account;
 import com.edwine.model.AccountViewBean;
+import com.edwine.model.entity.Favorites;
 import com.edwine.model.entity.Film;
 import java.io.Serializable;
 import javax.ejb.EJB;
@@ -55,6 +56,25 @@ public class FavoritesControllerBean implements Serializable{
             }
         } else {
             System.out.println("ERROR: No logged in user, can not add favorite!");
+        }
+
+        //System.out.println("Favorites: " + favDAO.getAccountsWhoFavoritedFilm(f).get(0).getAccount());
+    }
+    
+    public void removeFavorite() {
+        if (account.getLoggedInUser() != null) {
+            Account acc = accDAO.getAccountMatchingUsername(account.getLoggedInUser());
+
+            if (acc == null) {
+                System.out.println("ERROR: Could not find logged in account!");
+            } else {
+                Film f = filmDAO.findFilmsMatchingTitle(favoritesBackingBean.getFilm().getTitle()).get(0);
+                Favorites favorite = favDAO.getFavourite(acc, f);
+                favDAO.remove(favorite);
+                System.out.println("SUCCESS: " + f.getTitle() + " removed as favorite for user " + acc.getUsername());
+            }
+        } else {
+            System.out.println("ERROR: No logged in user, can not remove favorite!");
         }
 
         //System.out.println("Favorites: " + favDAO.getAccountsWhoFavoritedFilm(f).get(0).getAccount());
